42556ca0ae828b9cdaf67aab8fd92cae75d7a418
Simon Huang <418223521qq.com>
2026-06-16 23:19:40 +0800

    修改用户实体类，修改readme文件

--- User.java
+++ User.java
@@ -1,4 +1,8 @@
 public class User {
     private String username;
     private String password;
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
